1f9f50cae5f4837905edb12fd6ae280b30ec987a
BachtiarAH <bachtiarah73@gmail.com>
2023-04-14 21:26:44 +0700

    [done] detail packet camping done

---
+++ app/Http/Controllers/ControllerPacket.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerPacket extends Controller
+{
+    //
+    public function index()
+    {
+        $data = [
+            [
+                'id' => '1',
+                'name' => 'standar',
+                'price' => 1500000,
+                'img' => 'https://down-id.img.susercontent.com/file/9e7d9d14b0cbc3d28b7d73a6aa04547d',
+                'list' => [
+                    'tenda 2',
+                    'kursi lipat 1',
+                    'Matras 1',
+                    'Lampu Tenda 1',
+                    'Kompor dan Gas Kaleng 1',
+                    'panci',
+                    'Cangkir 2',
+                    'Sendok Garbu'
+                ],
+            ],
+            [
+                'id' => '2',
+                'name' => 'deluxe',
+                'price' => 2500000,
+                'img' => 'https://cdn.shopify.com/s/files/1/2216/6909/products/genie_20210427164153631_5139053899_1c9c42a7-fee8-4260-8472-88efb066400f.jpg?v=1620094884',
+                'list' => [
+                    'tenda 4',
+                    'kursi lipat 2',
+                    'Matras 2',
+                    'Lampu Tenda 2',
+                    'Kompor dan Gas Kaleng 2',
+                    'panci besar',
+                    'Cangkir 4',
+                    'Sendok Garpu'
+                ]
+            ],
+            [
+                'id' => '3',
+                'name' => 'premium',
+                'price' => 4000000,
+                'img' => 'https://cdn.shopify.com/s/files/1/2216/6909/products/VILLAGE17Artboard5.jpg?v=1675345647',
+                'list' => [
+                    'tenda 6',
+                    'kursi lipat 4',
+                    'Matras 4',
+                    'Lampu Tenda 3',
+                    'Kompor dan Gas Kaleng 3',
+                    'panci besar',
+                    'Cangkir 6',
+                    'Sendok Garpu'
+                ],
+            ],
+            [
+                'id' => '4',
+                'name' => 'economy',
+                'price' => 1000000,
+                'img' => 'https://sc04.alicdn.com/kf/H4ef996a085ef439798e111e4cbc1a4baE.png',
+                'list' => [
+                    'tenda 1',
+                    'kursi lipat 1',
+                    'Matras 1',
+                    'Lampu Tenda 1',
+                    'Kompor dan Gas Kaleng 1',
+                    'panci kecil',
+                    'Cangkir 1',
+                    'Sendok Garpu'
+                ],
+            ],
+            [
+                'id' => '5',
+                'name' => 'family',
+                'price' => 5000000,
+                'img' => 'https://speedsindo.com/wp-content/uploads/2022/11/%E4%B8%BB%E5%9B%BE-01-24.jpg',
+                'list' => [
+                    'tenda 8',
+                    'kursi lipat 6',
+                    'Matras 6',
+                    'Lampu Tenda 4',
+                    'Kompor dan Gas Kaleng 4',
+                    'panci besar',
+                    'Cangkir 8',
+                    'Sendok Garpu'
+                ],
+            ],
+            [
+                'id' => '6',
+                'name' => 'backpacker',
+                'price' => 750000,
+                'img' => 'https://images-cdn.ubuy.co.id/63580acd30e4a434e572e224-1-2-person-backpacking-tent-and-camping.jpg',
+                'list' => [
+                    'tenda 1',
+                    'sleeping bag',
+                    'kompor portable',
+                    'Gas Kaleng',
+                    'sendok, garpu, pisau lipat'
+                ],
+            ]
+        ];
+
+        return view('transaction.pages.list-paket', ['data' => $data]);
+    }
+
+    public function getById($id)
+    {
+        $packet = [
+            'id' => '1',
+            'name' => 'standar',
+            'price' => 1500000,
+            'img' => 'https://down-id.img.susercontent.com/file/9e7d9d14b0cbc3d28b7d73a6aa04547d',
+            'summary' => 'Paket camping yang ditujukan untuk empat orang dengan fasilitas yang lengkap dan nyaman',
+            'description' => 'Paket camping yang ditujukan untuk empat orang dengan fasilitas
+            yang lengkap dan nyaman. serta juga menyediakan fasilitas tambahan seperti
+            toilet dan gratis pemsangan tenda.',
+            'list' => [
+                'tenda 2',
+                'kursi lipat 1',
+                'Matras 1',
+                'Lampu Tenda 1',
+                'Kompor dan Gas Kaleng 1',
+                'panci',
+                'Cangkir 2',
+                'Sendok Garbu'
+            ],
+        ];
+
+        $reviews = [
+            [
+                'id_review' => '1',
+                'name' => 'Mamang Racing',
+                'img' => 'https://qph.cf2.quoracdn.net/main-qimg-c2108cee7b7917f80d8f6182a7a0a8ee-lq',
+                'star' => 5,
+                'comment' => 'First time mengunjungi wisata muara mbaduk merupakan pengalaman yang mengesankan buat saya.
+                Kunjungan saya kali ini untuk berkemah dengan pacar saya. Ada berbagai macam pilihan layanan camping yang
+                disediakan di wisata ini.'
+            ],
+            [
+                'id_review' => '2',
+                'name' => 'Nadia Sari',
+                'img' => 'https://example.com/nadia-sari.jpg',
+                'star' => 4,
+                'comment' => 'Wisata Muara Mbaduk sangat indah dan cocok untuk menghabiskan waktu liburan bersama keluarga. Pemandangannya sangat menakjubkan dan suasana di sini sangat tenang. Anak-anak saya juga senang bermain di area bermain yang disediakan.'
+            ],
+            [
+                'id_review' => '3',
+                'name' => 'Budi Hermawan',
+                'img' => 'https://example.com/budi-hermawan.jpg',
+                'star' => 3,
+                'comment' => 'Saya mengunjungi Wisata Muara Mbaduk pada akhir pekan. Lokasinya cukup mudah diakses dan parkirannya luas. Sayangnya, fasilitas yang ada masih terbatas, seperti toilet dan tempat makan. Namun, pemandangan alamnya sangat bagus dan cocok untuk berfoto-foto.'
+            ],
+            [
+                'id_review' => '4',
+                'name' => 'Rina Wijayanti',
+                'img' => 'https://example.com/rina-wijayanti.jpg',
+                'star' => 2,
+                'comment' => 'Saya merasa kurang puas dengan pengalaman saya di Wisata Muara Mbaduk. Pemandangan bagus, tapi fasilitasnya kurang memadai. Tempatnya juga agak terpencil dan jalan menuju ke sana cukup rusak. Harga tiket masuknya juga tergolong mahal.'
+            ]
+
+
+
+        ];
+        return view('transaction.pages.detail-packet', ['packet' => $packet, 'reviews' => $reviews]);
+    }
+
+    public function getMoreReviews()
+    {
+        $reviews = [
+            ['id_review' => '5',    'name' => 'Hendra Pratama',    'img' => 'https://example.com/hendra-pratama.jpg',    'star' => 3,    'comment' => 'Saya memiliki pengalaman yang cukup baik di Wisata Muara Mbaduk. Pemandangan yang indah dan suasana yang tenang. Namun, fasilitas yang disediakan masih perlu ditingkatkan.'],
+            ['id_review' => '6',    'name' => 'Siti Nurhayati',    'img' => 'https://example.com/siti-nurhayati.jpg',    'star' => 5,    'comment' => 'Saya sangat terkesan dengan Wisata Muara Mbaduk. Pemandangan alamnya sangat memukau dan fasilitas yang ada sudah cukup baik. Harga tiket masuknya juga cukup terjangkau.'],
+            [
+                'id_review' => '7',
+                'name' => 'Budi Setiawan',
+                'img' => 'https://example.com/budi-setiawan.jpg',
+                'star' => 4,
+                'comment' => 'Wisata Muara Mbaduk adalah tempat yang cocok untuk bersantai dan menikmati alam. Fasilitas yang ada sudah cukup lengkap dan pelayanan yang ramah. Namun, akses jalan menuju ke sana masih perlu diperbaiki.'
+            ],
+            [
+                'id_review' => '8',
+                'name' => 'Linda Sari',
+                'img' => 'https://example.com/linda-sari.jpg',
+                'star' => 1,
+                'comment' => 'Saya sangat kecewa dengan pengalaman saya di Wisata Muara Mbaduk. Pemandangan bagus, tapi fasilitasnya sangat minim dan kurang terawat. Harga tiket masuknya terlalu mahal untuk fasilitas yang diberikan.'
+            ],
+            ['id_review' => '9',    'name' => 'Ahmad Santoso',    'img' => 'https://example.com/ahmad-santoso.jpg',    'star' => 3,    'comment' => 'Saya merasa cukup puas dengan kunjungan saya ke Wisata Muara Mbaduk. Pemandangan alamnya indah, namun fasilitas yang ada masih perlu ditingkatkan. Harga tiket masuknya cukup wajar.']
+
+        ];
+
+        return json_encode($reviews);
+    }
+}
---
+++ resources/views/transaction/pages/detail-packet.blade.php
@@ -0,0 +1,152 @@
+@extends('layouts.user.app')
+@section('conten')
+<x-parts.header>
+    <h1 class="text-text-white font-bold text-5xl ">Paket untuk Camping</h1>
+    </x-parts-header>
+    <main class="p-24 ">
+        <section class="flex">
+            <img class="w-80 h-80 grow-0" src="{{$packet['img']}}" alt="">
+            <section class="ml-20 flex-col justify-start">
+                <h2 class="text-4xl font-bold capitalize">{{$packet['name']}}</h2>
+                <h3 class="text-text-blue text-4xl ">RP. {{$packet['price']}}</h3>
+                <summary class="text-text-black list-none max-w-xl mt-6">{{$packet['summary']}}</summary>
+                <a href=""><button class="bg-text-blue text-left py-4 px-10 rounded text-white font-bold my-6">Pesan Sekarang</button></a>
+                <div>
+                    <h3 class="font-bold pb-6">Description</h3>
+                    <p class="pb-6">{{$packet['description']}}</p>
+                    <ul class="pl-4 list-disc">
+                        @foreach($packet['list'] as $list)
+                        <li>{{$list}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </section>
+        </section>
+        <section class="mt-20">
+            <h2 class="font-bold text-xl mb-14">Ulasan</h2>
+            <div id="container" class="flex flex-col gap-6">
+                @foreach($reviews as $review)
+                <article class="flex ">
+                    <div class="w-24 shrink-0 h-24">
+                        <img class="rounded-full  w-full h-full " src="{{$review['img']}}" alt="">
+                    </div>
+                    <div class="mt-4 ml-8">
+                        <h3>{{$review['name']}}</h3>
+                        <div class="flex">
+                            @for($i = 0; $i < 5; $i++) <svg class="{{$i<=$review['star']? 'fill-yellow-300 stroke-yellow-300' : 'fill-none stroke-gray-600'}} " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="F7AE40" stroke-width="0.5" stroke-linecap="sharp" stroke-linejoin="sharp" class="feather feather-star">
+                                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                                @endfor
+
+                        </div>
+                        <p class="text-text-black mt-4">{{$review['comment']}}</p>
+                    </div>
+                </article>
+                @endforeach
+            </div>
+            <div class="mt-20 flex justify-center">
+                <button onclick="loadMoreComment(this)" class="py-4 px-8 bg-text-blue text-text-white font-bold rounded">Muat Lebih Banyak</button>
+            </div>
+        </section>
+
+    </main>
+    @endsection
+    @section('js')
+    <script>
+        function appendNewsComment(parentElement, data) {
+            // Create article element
+            const article = document.createElement('article');
+            article.classList.add('flex');
+
+            // Create div element for image
+            const imgDiv = document.createElement('div');
+            imgDiv.classList.add('w-24', 'shrink-0', 'h-24');
+            article.appendChild(imgDiv);
+
+            // Create img element
+            const img = document.createElement('img');
+            img.classList.add('rounded-full', 'w-full', 'h-full');
+            img.src = data.img;
+            img.alt = '';
+            imgDiv.appendChild(img);
+
+            // Create div element for text content
+            const textDiv = document.createElement('div');
+            textDiv.classList.add('mt-4', 'ml-8');
+            article.appendChild(textDiv);
+
+            // Create h3 element for name
+            const nameH3 = document.createElement('h3');
+            nameH3.textContent = data.name;
+            textDiv.appendChild(nameH3);
+
+            // Create div element for star rating
+            const starDiv = document.createElement('div');
+            starDiv.classList.add('flex');
+            textDiv.appendChild(starDiv);
+
+            // Loop to create svg elements for star rating
+            for (let i = 0; i < 5; i++) {
+                const starSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                starSvg.setAttribute('width', '24');
+                starSvg.setAttribute('height', '24');
+                starSvg.setAttribute('viewBox', '0 0 24 24');
+                starSvg.setAttribute('fill', i <= data.star ? 'yellow' : 'none');
+                starSvg.setAttribute('stroke', i <= data.star ? 'F7AE40' : 'gray');
+                starSvg.setAttribute('stroke-width', '0.5');
+                starSvg.setAttribute('stroke-linecap', 'sharp');
+                starSvg.setAttribute('stroke-linejoin', 'sharp');
+                starSvg.classList.add('feather', 'feather-star');
+
+                const starPolygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+                starPolygon.setAttribute('points', '12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2');
+                starSvg.appendChild(starPolygon);
+
+                starDiv.appendChild(starSvg);
+            }
+
+            // Create p element for comment
+            const commentP = document.createElement('p');
+            commentP.classList.add('text-text-black', 'mt-4');
+            commentP.textContent = data.comment;
+            textDiv.appendChild(commentP);
+
+            parentElement.appendChild(article);
+
+        }
+
+        function loadMoreComment(param) {
+            // cardNews = document.getElementsByClassName('getElementsByClassName');
+            container = document.getElementById("container");
+
+            var xhr = new XMLHttpRequest();
+
+
+            var url = "{{route('packet.more')}}";
+            xhr.open('GET', url, true);
+
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    for (let index = 0; index < response.length; index++) {
+
+                        appendNewsComment(container, response[index]);
+                    }
+                } else if (xhr.readyState === XMLHttpRequest.DONE) {
+                    console.error('Error retrieving data: ' + xhr.statusText);
+                } else {
+                    param.innerHTML = "loading data"
+                }
+            };
+
+            // Send the AJAX request
+            xhr.send();
+
+            param.hidden = true;
+
+
+
+        }
+
+    </script>
+    @endsection
